69920c4d599740eb41f724b0ed4f226c69be8460
Amr Dweikat <Amr Dweikat@192.168.1.6>
2019-02-20 18:58:45 +0300

    new

--- src/ForTesting/FirstTest.java
+++ src/ForTesting/FirstTest.java
@@ -17,25 +17,16 @@ public class FirstTest {
 		shoppingcart sc = new shoppingcart();
 		assertTrue(0 == sc.get_product_count());
 		}
-	
-	shoppingcart test ;
-	@Before
-	public void setUp()  {
-	test = new shoppingcart();
-	}
 	@Test
 	public void test2() {
+		shoppingcart test = new shoppingcart();
 		test.add_book(1,"Java Book",127);//add_book(amount,name,price) for one book
 		assertTrue(1 == test.get_product_count());
 		assertTrue(127 == test.get_total_value());
 		}
-	@After
-	public void tearDown() {
-		test.delete();
-	}
 	@Test
 	public void test3() {
-		test = new shoppingcart();
+		shoppingcart test = new shoppingcart();
 		test.add_book(1,"Java Book",127);//add_book(amount,name,price) for one book
 		test.add_book(1,"Web design Book",100);//add_book(amount,name,price) for one book
 		assertTrue(2 == test.get_product_count());
